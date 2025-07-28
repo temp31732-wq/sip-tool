a650954a1cf69d4dfb90c6a14125b0a6153b4306
temp31732-wq <temp31732@gmail.com>
2025-07-28 14:25:33 +0530

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
 import { useFrameworkReady } from '../src/hooks/useFrameworkReady'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+
 
 export default function RootLayout() {
   useFrameworkReady();
